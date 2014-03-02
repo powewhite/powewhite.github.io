b027a76c740f5c3d73346fc2a3b2f6eb481c2aae
danielodd <d.odd@nib.com.au>
2014-03-03 10:18:59 +1100

    Revert "Added website"
    This reverts commit d380a175f73d976b614eede3be3edc2ca0b9d1e3.

--- js/TopNavigation.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-     $("#topNavTemp").load("Header.html #topNav",function(){
-        var template = document.getElementById('topNav').innerHTML;
-        var output = Mustache.render(template);
-        $("#displayTopNavTemp").html(output);
-    });
-})
